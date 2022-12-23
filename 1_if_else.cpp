979b9725a2dbc9719cd409a942630a1bdf93c54e
Satvik <iamsatvik77@gmail.com>
2022-12-24 04:41:31 +0530

    modified the code

--- 1_if_else.cpp
+++ 1_if_else.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout<<"Hello World";
     int a,b;
+    cout<<"Enter the first number: ";
     cin>>a;
+    cout<<"Enter the second number: ";
     cin>>b;
-    cout<<a<<b;
+    if (a<b){
+        cout<<"A is less then B";
+    }
+    else if (a==b){
+        cout<<"A is equal to B";
+    }
+    else{
+        cout<<"B is less then A";
+    }
 return 0;
 }
