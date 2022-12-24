0373b91067379a501582d6996b79f76591f2fa66
Satvik <iamsatvik77@gmail.com>
2022-12-24 17:31:36 +0530

    fibonacci series in cpp

---
+++ fibo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1=0;
+    int num2=1;
+    int sum;
+    int terms;
+    cout<<"How many terms do you want: ";
+    cin>>terms;
+    for(int i=0;i<terms;i++){
+        cout<<num1<<" ";
+        sum=num1+num2;
+        num1=num2;
+        num2=sum;
+    }
+return 0;
+}
