54c4d1eb2af4b4d4e11d1abb3cbaec388cd75622
Satvik <iamsatvik77@gmail.com>
2022-12-24 16:37:03 +0530

    pascals triangle

--- 1_pattern_01.cpp
+++ 1_pattern_01.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int r;
-    cout<<"Enter the number of rows: ";
+    int r,space,j,start;
     cin>>r;
-    for (int i=1;i<=r;i++){
-        for (int j=0;j<r;j++){        
-            cout<<i;
+    int i=1;
+    while(i<=r){
+        space=r-i;
+        while(space){
+            cout<<" ";
+            space-=1;
+        }
+        j=1;
+        while(j<=i){
+            cout<<j;
+            j+=1;
+        }
+
+        start=i-1;
+        while(start){
+            cout<<start;
+            start=start-1;
         }
         cout<<"\n";
+        i+=1;
     }
 return 0;
 }
