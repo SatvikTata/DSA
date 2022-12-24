6462636605237edc9c087cea9568af4ff44cc43d
Satvik <iamsatvik77@gmail.com>
2022-12-24 20:29:49 +0530

    binary to decimal

---
+++ btd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int main(){
+    int n,i=0;
+    int ans=0;
+    cout<<"Enter the binary number: ";
+    cin>>n;
+    while(n!=0){
+        int digit=n%10;
+        ans=(digit*pow(2,i))+ans;
+        i++;
+        n=n/10;
+    }
+    cout<<"The decimal equivalent is: "<<ans;
+return 0;
+}
