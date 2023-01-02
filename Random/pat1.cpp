f714622804ea4be57e610182d1003116a2bdfdbc
Satvik <iamsatvik77@gmail.com>
2023-01-03 01:45:52 +0530

    printing patterns

---
+++ Random/pat1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter the number of rows you want: ";
+    cin>>a;
+    for(int i=1;i<=a;i++){
+        for(int j=1;j<i;j++){
+            cout<<" ";
+        }
+        for(int k=1;k<=((i*(-2))+((2*a)+1));k++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+return 0;
+}
---
+++ Random/pat2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter the number of rows you want: ";
+    cin>>a;
+return 0;
+}
