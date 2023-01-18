bc455d52a460cc8d155055aa15fcabbcd0fc5f8a
Satvik <iamsatvik77@gmail.com>
2023-01-18 21:13:45 +0530

    Basics of array

--- Day_3/arr_1.cpp
+++ Day_3/arr_1.cpp
@@ -6,7 +6,11 @@ int main(){
         cin>>arr[i];
     }
     for(int i=0;i<15;i++){
-        cout<<arr[i];
+        cout<<arr[i]<<" ";
     }
+
+    int sizeofArray;
+    sizeofArray=sizeof(arr)/sizeof(int);
+    cout<<endl<<sizeofArray<<endl;
 return 0;
 }
