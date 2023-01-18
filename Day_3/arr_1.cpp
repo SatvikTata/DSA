9f4c9c705af9cb4ba493511d3241691d6f62bd20
Satvik <iamsatvik77@gmail.com>
2023-01-18 18:55:06 +0530

    Created Day_3 folder

---
+++ Day_3/arr_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[15];
+    for(int i=0;i<15;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<15;i++){
+        cout<<arr[i];
+    }
+return 0;
+}
